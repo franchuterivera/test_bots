61490f5f9b8bfa2974e4b0442cac537b6cfd7c61
chico <francisco.rivera.valverde@gmail.com>
2021-02-13 00:55:55 +0100

    random

--- build_tools/test_source.sh
+++ build_tools/test_source.sh
@@ -5,13 +5,13 @@ set -x
 
 # Test the distribution on an isolated venv
 # Directory structure is such that
-# PWD=random_forest_run/random_forest_run/<repo>
+# PWD=test_bots/test_bots/<repo>
 cd ../../
 
 python -m venv test_env
 source test_env/bin/activate
 
 python -m pip install pytest "numpy<=1.19"
-python -m pip install random_forest_run/random_forest_run/dist/*.tar.gz
+python -m pip install test_bots/test_bots/dist/*.tar.gz
 
-pytest random_forest_run/random_forest_run/tests/pyrfr_unit_test_binary_regression_forest_transformed_data.py
+pytest test_bots/test_bots/tests/pyrfr_unit_test_binary_regression_forest_transformed_data.py
